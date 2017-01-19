63238106a3874fb56660cdef1ba3b1f86fddfab5
reinvdwoerd <reinvanderwoerd@me.com>
2017-01-19 19:21:30 +0100

    -

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,2 +1,4 @@
 {:user {:plugins [[lein-midje "3.1.3"]
-                  [venantius/ultra "0.5.1"]]}}
+                  [venantius/ultra "0.5.1"]
+                  [com.billpiel/sayid "0.0.11"]
+                  [lein-try "0.4.3"]]}}
